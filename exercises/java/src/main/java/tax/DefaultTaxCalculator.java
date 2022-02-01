77878d5efd7b05c5b9d22813b3f37ddda65d9443
Philip <philip.ingham.phs@gmail.com>
2022-02-01 09:53:31 +0000

    Refactor DefaultTaxCalculator

--- exercises/java/src/main/java/tax/DefaultTaxCalculator.java
+++ exercises/java/src/main/java/tax/DefaultTaxCalculator.java
@@ -3,68 +3,77 @@ package tax;
 public class DefaultTaxCalculator extends TaxCalculator {
     @Override
     int calculateTax(Vehicle vehicle) {
-        if (vehicle.getFuelType().equals(FuelType.ALTERNATIVE_FUEL)) {
-            return calculateTaxAlternativeFuel(vehicle.getCo2Emissions());
-        }
+
         int co2Emissions = vehicle.getCo2Emissions();
         switch (vehicle.getFuelType()) {
             case PETROL:
-                if (co2Emissions == 0) {
-                    return 0;
-                } else if (co2Emissions >= 1 && co2Emissions <= 50){
-                    return 10;
-                } else if (co2Emissions >= 51 && co2Emissions <= 75) {
-                    return 25;
-                } else if (co2Emissions >= 76 && co2Emissions <= 90) {
-                    return 105;
-                } else if (co2Emissions >= 91 && co2Emissions <= 100) {
-                    return 125;
-                } else if (co2Emissions >= 101 && co2Emissions <= 110) {
-                    return 145;
-                } else if (co2Emissions >= 111 && co2Emissions <= 130) {
-                    return 165;
-                } else if (co2Emissions >= 131 && co2Emissions <= 150) {
-                    return 205;
-                } else if (co2Emissions >= 151 && co2Emissions <= 170) {
-                    return 515;
-                } else if (co2Emissions >= 171 && co2Emissions <= 190) {
-                    return 830;
-                } else if (co2Emissions >= 191 && co2Emissions <= 225) {
-                    return 1240;
-                } else if (co2Emissions >= 226 && co2Emissions <= 255) {
-                    return 1760;
-                } else {
-                    return 2070;
-                }
+                return calculateTaxPetrol(co2Emissions);
             case DIESEL:
-                if (co2Emissions == 0) {
-                    return 0;
-                } else if (co2Emissions >= 1 && co2Emissions <= 50){
-                    return 25;
-                } else if (co2Emissions >= 51 && co2Emissions <= 75) {
-                    return 105;
-                } else if (co2Emissions >= 76 && co2Emissions <= 90) {
-                    return 125;
-                } else if (co2Emissions >= 91 && co2Emissions <= 100) {
-                    return 145;
-                } else if (co2Emissions >= 101 && co2Emissions <= 110) {
-                    return 165;
-                } else if (co2Emissions >= 111 && co2Emissions <= 130) {
-                    return 205;
-                } else if (co2Emissions >= 131 && co2Emissions <= 150) {
-                    return 515;
-                } else if (co2Emissions >= 151 && co2Emissions <= 170) {
-                    return 830;
-                } else if (co2Emissions >= 171 && co2Emissions <= 190) {
-                    return 1240;
-                } else if (co2Emissions >= 191 && co2Emissions <= 225) {
-                    return 1760;
-                } else if (co2Emissions >= 226 && co2Emissions <= 255) {
-                    return 2070;
-                } else {
-                    return 2070;
-                }
+                return calculateTaxDiesel(co2Emissions);
+            case ALTERNATIVE_FUEL:
+                return calculateTaxAlternativeFuel(co2Emissions);
             default: return 0;
+        }
+    }
+
+
+    public int calculateTaxPetrol(int co2Emissions){
+        if (co2Emissions == 0) {
+            return 0;
+        } else if (co2Emissions >= 1 && co2Emissions <= 50){
+            return 10;
+        } else if (co2Emissions >= 51 && co2Emissions <= 75) {
+            return 25;
+        } else if (co2Emissions >= 76 && co2Emissions <= 90) {
+            return 105;
+        } else if (co2Emissions >= 91 && co2Emissions <= 100) {
+            return 125;
+        } else if (co2Emissions >= 101 && co2Emissions <= 110) {
+            return 145;
+        } else if (co2Emissions >= 111 && co2Emissions <= 130) {
+            return 165;
+        } else if (co2Emissions >= 131 && co2Emissions <= 150) {
+            return 205;
+        } else if (co2Emissions >= 151 && co2Emissions <= 170) {
+            return 515;
+        } else if (co2Emissions >= 171 && co2Emissions <= 190) {
+            return 830;
+        } else if (co2Emissions >= 191 && co2Emissions <= 225) {
+            return 1240;
+        } else if (co2Emissions >= 226 && co2Emissions <= 255) {
+            return 1760;
+        } else {
+            return 2070;
+        }
+    }
+
+    public int calculateTaxDiesel(int co2Emissions) {
+        if (co2Emissions == 0) {
+            return 0;
+        } else if (co2Emissions >= 1 && co2Emissions <= 50){
+            return 25;
+        } else if (co2Emissions >= 51 && co2Emissions <= 75) {
+            return 105;
+        } else if (co2Emissions >= 76 && co2Emissions <= 90) {
+            return 125;
+        } else if (co2Emissions >= 91 && co2Emissions <= 100) {
+            return 145;
+        } else if (co2Emissions >= 101 && co2Emissions <= 110) {
+            return 165;
+        } else if (co2Emissions >= 111 && co2Emissions <= 130) {
+            return 205;
+        } else if (co2Emissions >= 131 && co2Emissions <= 150) {
+            return 515;
+        } else if (co2Emissions >= 151 && co2Emissions <= 170) {
+            return 830;
+        } else if (co2Emissions >= 171 && co2Emissions <= 190) {
+            return 1240;
+        } else if (co2Emissions >= 191 && co2Emissions <= 225) {
+            return 1760;
+        } else if (co2Emissions >= 226 && co2Emissions <= 255) {
+            return 2070;
+        } else {
+            return 2070;
         }
     }
 
